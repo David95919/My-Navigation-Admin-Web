b932585eccc7d368e2e23ca1b77806bc97c668a2
David95919 <150328571+David95919@users.noreply.github.com>
2024-08-23 20:35:39 +0800

    fix: admin display grid

--- src/views/admin/ManageContainer.vue
+++ src/views/admin/ManageContainer.vue
@@ -11,137 +11,129 @@ const handleOpenSettings = () => {
 }
 </script>
 <template>
-  <el-container class="common-layout">
-    <el-card class="aside-card" shadow="always">
-      <el-aside class="aside" style="width: auto">
-        <el-menu
-          class="menu"
-          :default-active="route.path"
-          :default-openeds="['navigation', 'search']"
-          router
-          collapse-transition
-        >
-          <el-menu-item index="/admin/home">
-            <el-icon>
-              <i-material-symbols-home-outline-rounded />
-            </el-icon>
-            <span>{{ $t('manage.home') }}</span>
-          </el-menu-item>
-          <el-sub-menu index="navigation">
-            <template #title>
-              <el-icon>
-                <i-material-symbols-assistant-navigation-outline-rounded />
-              </el-icon>
-              <span>{{ $t('manage.navigation') }}</span>
-            </template>
-            <el-menu-item index="/admin/navigation">
-              <el-icon>
-                <i-material-symbols-assistant-navigation-outline-rounded />
-              </el-icon>
-              <span>{{ $t('manage.navigation') }}</span>
-            </el-menu-item>
-            <el-menu-item index="/admin/category">
-              <el-icon>
-                <i-material-symbols-category-outline-rounded />
-              </el-icon>
-              <span>{{ $t('manage.category') }}</span>
-            </el-menu-item>
-            <el-menu-item index="/admin/tag">
-              <el-icon>
-                <i-material-symbols-tag-rounded />
-              </el-icon>
-              <span>{{ $t('manage.tag') }}</span>
-            </el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="search">
-            <template #title>
-              <el-icon>
-                <i-material-symbols-search-rounded />
-              </el-icon>
-              <span>{{ $t('manage.search') }}</span>
-            </template>
-            <el-menu-item index="/admin/search">
-              <el-icon>
-                <i-material-symbols-search-rounded />
-              </el-icon>
-              <span>{{ $t('manage.search') }}</span>
-            </el-menu-item>
-            <el-menu-item index="/admin/searchCategory">
-              <el-icon>
-                <i-material-symbols-category-outline-rounded />
-              </el-icon>
-              <span>{{ $t('manage.searchCategory') }}</span>
-            </el-menu-item>
-          </el-sub-menu>
-          <el-menu-item index="/admin/user">
-            <el-icon>
-              <i-material-symbols-user-attributes-outline-rounded />
-            </el-icon>
-            <span>{{ $t('manage.user') }}</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+  <div class="wrapper">
+    <el-card class="header">
+      <div class="title">My-Navigation-Admin</div>
+      <el-button circle class="settings" @click="handleOpenSettings">
+        <el-icon>
+          <i-material-symbols-settings />
+        </el-icon>
+      </el-button>
     </el-card>
-    <div class="container">
-      <el-card class="header" shadow="always">
-        <div class="title">My-Navigation-Admin</div>
-        <el-button circle class="settings" @click="handleOpenSettings">
+    <el-menu
+      class="sidebar"
+      :default-active="route.path"
+      :default-openeds="['navigation', 'search']"
+      router
+      collapse-transition
+    >
+      <el-menu-item index="/admin/home">
+        <el-icon>
+          <i-material-symbols-home-outline-rounded />
+        </el-icon>
+        <span>{{ $t('manage.home') }}</span>
+      </el-menu-item>
+      <el-sub-menu index="navigation">
+        <template #title>
           <el-icon>
-            <i-material-symbols-settings />
+            <i-material-symbols-assistant-navigation-outline-rounded />
           </el-icon>
-        </el-button>
-      </el-card>
-      <div class="main">
-        <el-scrollbar height="89vh">
-          <router-view></router-view>
-        </el-scrollbar>
-      </div>
+          <span>{{ $t('manage.navigation') }}</span>
+        </template>
+        <el-menu-item index="/admin/navigation">
+          <el-icon>
+            <i-material-symbols-assistant-navigation-outline-rounded />
+          </el-icon>
+          <span>{{ $t('manage.navigation') }}</span>
+        </el-menu-item>
+        <el-menu-item index="/admin/category">
+          <el-icon>
+            <i-material-symbols-category-outline-rounded />
+          </el-icon>
+          <span>{{ $t('manage.category') }}</span>
+        </el-menu-item>
+        <el-menu-item index="/admin/tag">
+          <el-icon>
+            <i-material-symbols-tag-rounded />
+          </el-icon>
+          <span>{{ $t('manage.tag') }}</span>
+        </el-menu-item>
+      </el-sub-menu>
+      <el-sub-menu index="search">
+        <template #title>
+          <el-icon>
+            <i-material-symbols-search-rounded />
+          </el-icon>
+          <span>{{ $t('manage.search') }}</span>
+        </template>
+        <el-menu-item index="/admin/search">
+          <el-icon>
+            <i-material-symbols-search-rounded />
+          </el-icon>
+          <span>{{ $t('manage.search') }}</span>
+        </el-menu-item>
+        <el-menu-item index="/admin/searchCategory">
+          <el-icon>
+            <i-material-symbols-category-outline-rounded />
+          </el-icon>
+          <span>{{ $t('manage.searchCategory') }}</span>
+        </el-menu-item>
+      </el-sub-menu>
+      <el-menu-item index="/admin/user">
+        <el-icon>
+          <i-material-symbols-user-attributes-outline-rounded />
+        </el-icon>
+        <span>{{ $t('manage.user') }}</span>
+      </el-menu-item>
+    </el-menu>
+    <div class="content">
+      <router-view></router-view>
     </div>
     <SettingsDialogComponent ref="settingsDialog"></SettingsDialogComponent>
-  </el-container>
+  </div>
 </template>
 <style scoped lang="less">
-.common-layout {
-  width: 100vw;
-  height: 100vh;
+.wrapper {
+  display: grid;
+  min-height: 100vh;
 
-  .aside-card {
-    width: 12vw;
-    max-width: 12vw;
-    min-width: 130px;
-    margin: 10px 0 10px 10px;
+  /* 使用 minmax 来确保 sidebar 有一个最小宽度，内容区域填充剩余空间 */
+  grid-template-columns: minmax(200px, auto) 1fr;
+  grid-template-rows: auto 1fr;
 
-    .aside {
-      width: 12vw;
+  grid-template-areas:
+    'sidebar header'
+    'sidebar content';
 
-      .el-menu {
-        border-right: 0;
-      }
-    }
+  overflow: hidden; /* 避免滚动条 */
+}
+
+.header {
+  position: relative;
+  grid-area: header;
+  width: 100%; /* 确保 header 的宽度自适应 */
+
+  .settings {
+    position: absolute;
+    top: 10px;
+    right: 10px;
   }
+}
 
-  .container {
-    width: 100%;
+.sidebar {
+  grid-area: sidebar;
+  max-width: 100%; /* 确保 sidebar 不会超出容器宽度 */
+}
 
-    .header {
-      position: relative;
-      margin: 10px;
+.content {
+  grid-area: content;
+  overflow: auto; /* 允许内容区域在必要时滚动 */
+  min-width: 0; /* 允许内容区域在必要时缩小 */
+}
 
-      .title {
-        font-size: 1.25em;
-        text-align: center;
-      }
-
-      .settings {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-      }
-    }
-
-    .main {
-      margin: 10px;
-    }
-  }
+.header,
+.sidebar,
+.content {
+  margin: 5px;
 }
 </style>
